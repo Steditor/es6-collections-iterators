510f9b975c026b626076f3adb955bfd95567fc1d
Stefan Neubert <Stefan.Neubert@student.hpi.de>
2015-07-05 15:39:39 +0200

    Add test for Set.prototype.keys()

---
+++ test/set.js
@@ -0,0 +1,49 @@
+require('../iterators.js');
+chai = require('chai');
+expect = chai.expect;
+
+var set, emptyset;
+
+describe('Set prototype functions', function(){
+  beforeEach(function(){
+    set = new Set();
+    set.add('a');
+    set.add('b');
+    set.add('c');
+    emptyset = new Set();
+  });
+
+  it('should iterate over keys of a filled set', function() {
+    var iterator = set.keys();
+    var first = iterator.next();
+    var second = iterator.next();
+    var third = iterator.next();
+    var fourth = iterator.next();
+    var fifth = iterator.next();
+
+    expect(first.done).to.be.false;
+    expect(first.value).to.equal('a');
+
+    expect(second.done).to.be.false;
+    expect(second.value).to.equal('b');
+
+    expect(third.done).to.be.false;
+    expect(third.value).to.equal('c');
+
+    expect(fourth.done).to.be.true;
+    expect(fourth.value).to.equal(undefined);
+
+    expect(fifth).to.deep.equal(fourth);
+  });
+
+  it('should iterate over keys of an empty set', function() {
+    var iterator = emptyset.keys();
+    var first = iterator.next();
+    var second = iterator.next();
+
+    expect(first.done).to.be.true;
+    expect(first.value).to.equal(undefined);
+
+    expect(second).to.deep.equal(first);
+  });
+});
