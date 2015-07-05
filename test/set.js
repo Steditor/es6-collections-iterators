1f517e5d2bf261a5ed1cd2d4b73572cbe5e0f97a
Stefan Neubert <Stefan.Neubert@student.hpi.de>
2015-07-05 15:40:27 +0200

    Add test for Set.prototype.values()

--- test/set.js
+++ test/set.js
@@ -46,4 +46,38 @@ describe('Set prototype functions', function(){
 
     expect(second).to.deep.equal(first);
   });
+
+  it('should iterate over values of a filled set', function() {
+    var iterator = set.values();
+    var first = iterator.next();
+    var second = iterator.next();
+    var third = iterator.next();
+    var fourth = iterator.next();
+    var fifth = iterator.next();
+
+    expect(first.done).to.be.false;
+    expect(first.value).to.equal('a');
+
+    expect(second.done).to.be.false;
+    expect(second.value).to.equal('b');
+
+    expect(third.done).to.be.false;
+    expect(third.value).to.equal('c');
+
+    expect(fourth.done).to.be.true;
+    expect(fourth.value).to.equal(undefined);
+
+    expect(fifth).to.deep.equal(fourth);
+  });
+
+  it('should iterate over values of an empty set', function() {
+    var iterator = emptyset.values();
+    var first = iterator.next();
+    var second = iterator.next();
+
+    expect(first.done).to.be.true;
+    expect(first.value).to.equal(undefined);
+
+    expect(second).to.deep.equal(first);
+  });
 });
