080d15459890cc6cda2aed925a868c378efdce26
Stefan Neubert <Stefan.Neubert@student.hpi.de>
2015-07-05 15:30:31 +0200

    Add test for Map.prototype.entries()

--- test/map.js
+++ test/map.js
@@ -80,4 +80,38 @@ describe('Map prototype functions', function(){
 
     expect(second).to.deep.equal(first);
   });
+
+  it('should iterate over entries of a filled map', function() {
+    var iterator = map.entries();
+    var first = iterator.next();
+    var second = iterator.next();
+    var third = iterator.next();
+    var fourth = iterator.next();
+    var fifth = iterator.next();
+
+    expect(first.done).to.be.false;
+    expect(first.value).to.deep.equal(['a', 1]);
+
+    expect(second.done).to.be.false;
+    expect(second.value).to.deep.equal(['b', 2]);
+
+    expect(third.done).to.be.false;
+    expect(third.value).to.deep.equal(['c', 3]);
+
+    expect(fourth.done).to.be.true;
+    expect(fourth.value).to.equal(undefined);
+
+    expect(fifth).to.deep.equal(fourth);
+  });
+
+  it('should iterate over entries of an empty map', function() {
+    var iterator = emptymap.entries();
+    var first = iterator.next();
+    var second = iterator.next();
+
+    expect(first.done).to.be.true;
+    expect(first.value).to.equal(undefined);
+
+    expect(second).to.deep.equal(first);
+  });
 });
