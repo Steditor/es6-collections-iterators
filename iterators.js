267d9b776091690059d4be68a22ff2fe8f8851cb
Stefan Neubert <Stefan.Neubert@student.hpi.de>
2015-07-06 09:57:59 +0200

    Check for corrupted iterator as found in Safari 7.1 and 8

--- iterators.js
+++ iterators.js
@@ -2,25 +2,33 @@
 
   //Polyfill global objects
   if (typeof Map != 'undefined') {
-    if (typeof Map.prototype.entries == 'undefined') {
+    var map = new Map();
+    if (typeof Map.prototype.entries == 'undefined'
+      || typeof map.entries().next == 'undefined') {
       Map.prototype.entries = mapEntries;
     }
-    if (typeof Map.prototype.values == 'undefined') {
+    if (typeof Map.prototype.values == 'undefined'
+      || typeof map.values().next == 'undefined') {
       Map.prototype.values = mapValues;
     }
-    if (typeof Map.prototype.keys == 'undefined') {
+    if (typeof Map.prototype.keys == 'undefined'
+      || typeof map.keys().next == 'undefined') {
       Map.prototype.keys = mapKeys;
     }
   }
 
   if (typeof Set != 'undefined') {
-    if (typeof Set.prototype.entries == 'undefined') {
+    var set = new Set();
+    if (typeof Set.prototype.entries == 'undefined'
+      || typeof set.entries().next == 'undefined') {
       Set.prototype.entries = setEntries;
     }
-    if (typeof Set.prototype.values == 'undefined') {
+    if (typeof Set.prototype.values == 'undefined'
+      || typeof set.values().next == 'undefined') {
       Set.prototype.values = setValues;
     }
-    if (typeof Set.prototype.keys == 'undefined') {
+    if (typeof Set.prototype.keys == 'undefined'
+      || typeof set.keys().next == 'undefined') {
       Set.prototype.keys = setValues;
     }
   }
